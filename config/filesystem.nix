efefbe3826bb418c4957b37ba84233f57f57a228
Givon Washington <gwashington@makenotion.com>
2025-09-29 10:07:35 -0700

    feat(filesystem): enable `project-nvim`

--- config/filesystem.nix
+++ config/filesystem.nix
@@ -2,9 +2,15 @@
   keymaps = [
     { mode = "n"; key = "<LEADER>e"; action = "<CMD>NvimTreeToggle<CR>"; }
   ];
-  plugins.nvim-tree = {
-    enable = true;
-    hijackNetrw = true;
-    updateFocusedFile.enable = true;
+  plugins = {
+    nvim-tree = {
+      enable = true;
+      hijackNetrw = true;
+      updateFocusedFile.enable = true;
+    };
+    project-nvim = {
+      enable = true;
+      enableTelescope = true;
+    };
   };
 }
